99c884f964252a04a363a06c66009e0d13bd0b52
Raphaël de Courville <groupes.raphael@gmail.com>
2025-12-06 20:38:01 +0100

    feat: create basic-instance-js template from instance

---
+++ templates/basic-instance-js/sketch.js
@@ -0,0 +1,17 @@
+// Instance mode allows multiple sketches on one page
+// and avoids polluting the global namespace
+
+const sketch = (p) => {
+  p.setup = () => {
+    p.createCanvas(400, 400);
+    p.background(220);
+  };
+
+  p.draw = () => {
+    // Draw a simple circle that follows the mouse
+    p.ellipse(p.mouseX, p.mouseY, 50, 50);
+  };
+};
+
+// Create a new p5 instance, passing in the sketch function
+new p5(sketch, 'sketch-container');
