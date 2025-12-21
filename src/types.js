6bcd96acb0e95b2bf987340a7c785d5e25ea70dc
nbogie <neillbogie@googlemail.com>
2025-12-21 02:44:34 +0000

    add some central types.js

---
+++ src/types.js
@@ -0,0 +1,29 @@
+
+/**
+ * @typedef {"cdn"|"local"} DeliveryMode
+ */
+/**
+ * @typedef {'basic' | 'standard' | 'custom'} SetupType
+ */
+/**
+ * @typedef {"javascript"|"typescript"} Language
+ */
+/**
+ * @typedef {"global"|"instance"} P5Mode
+ */
+
+/**
+ * @typedef {object} CliArgs
+ * @property {any[]} _
+ * @property {boolean} [verbose]
+ * @property {boolean} [yes]
+ * @property {string} [language]
+ * @property {string} [p5-mode]
+ * @property {string} [version]
+ * @property {string} [mode] 
+ * @property {string} [type] 
+ * @property {boolean} [types] 
+ * @property {string} [template] 
+ * @property {string} [git] 
+ * @property {boolean} [include-prerelease]
+ */
